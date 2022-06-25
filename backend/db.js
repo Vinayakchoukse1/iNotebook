1ee989fd74f637747207502894e1515f95c79bac
vinayak choukse <80781272+Vinayakchoukse1@users.noreply.github.com>
2022-06-25 10:09:38 +0530

    Database changed

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const connectToMongo = () => {
   const mongoURI =
-    "mongodb+srv://Vinayakchoukse1:Vinayak.1@cluster0.zbqyf.mongodb.net/?retryWrites=true&w=majority";
-  mongoose.connect(mongoURI, (err) => {
-    if (err) console.log("Failed to connect to database");
-    else console.log("Mongo connected :)");
-  });
-};
+    "mongodb://localhost:27017/"; // local databes MongoDB atlas
+    mongoose.connect(mongoURI, (err) => {
+      if (err) console.log("Failed to connect to database");
+      else console.log("Mongo connected :)");
+    });
+  };
+  
+  module.exports = connectToMongo;
 
-module.exports = connectToMongo;
+// Cloud MongoDB  
 
+// "mongodb+srv://Vinayakchoukse1:Vinayak.1@cluster0.zbqyf.mongodb.net/?retryWrites=true&w=majority";
 // mongodb+srv://arpandesai0:ad1234@cluster0.82awn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
